8c945d2df6d5ac3d5a4568c78e608fbb5e4af5aa
faith <89432125+faithchongrui@users.noreply.github.com>
2022-07-02 09:11:41 +0800

    Initial Commit

---
+++ todoist/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  todoist
+//
+//  Created by FAITH CHONG RUI EN stu on 2/7/22.
+//
+
+import Foundation
+
+struct Todo {
+    var title: String
+    var isCompleted: Bool = false
+}
---
+++ todoist/todoistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoistApp.swift
+//  todoist
+//
+//  Created by FAITH CHONG RUI EN stu on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct todoistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
